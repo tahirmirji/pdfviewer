0a1433f627abee7e6861e2a5d94c45a71ac08cc4
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2021-04-29 12:45:19 +0530

    Image Decoration

--- lib/main.dart
+++ lib/main.dart
@@ -33,38 +33,110 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('PDF Module'),
+              accountEmail: Text('nextingoindia@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                child: Text(
+                  'N',
+                  style: TextStyle(fontSize: 30.0),
+                ),
+                backgroundColor: Colors.white,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text('Notifications'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+            ListTile(
+              leading: Icon(Icons.info_outlined),
+              title: Text('About'),
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer_outlined),
+              title: Text('FAQ'),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text("PDF Viewer Module"),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                child: IconButton(
+                  onPressed: (){},
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+              SizedBox(width: 10.0,),
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                child: IconButton(
+                  tooltip: 'Notifications',
+                  onPressed: () {},
+                  icon: Icon(Icons.notifications),
+                  iconSize: 20.0,
+                ),
+              ),
+              SizedBox(width: 5.0,),
+            ],
+          ),
+        ],
       ),
       body: Container(
         child: Center(
           child: isLoading
               ? CircularProgressIndicator()
               : Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   mainAxisSize: MainAxisSize.max,
                   children: [
+                    Image.network('https://www.pccsuk.com/blog/wp-content/uploads/2016/11/searching.png'),
                     ElevatedButton(
                       onPressed: () {
                         loadFromAsset();
                       },
                       child: Text(
-                        " Load local PDF ",
+                        "View local PDF ",
                         style: TextStyle(fontSize: 20.0),
                       ),
                     ),
-                    
                     ElevatedButton(
                       onPressed: () {
                         loadFromURL();
                       },
                       child: Text(
-                        "Load PDF via URL",
+                        "View PDF in Network",
                         style: TextStyle(fontSize: 20.0),
                       ),
                     ),
-                    Center(child: Text('${Constants.pdfURL}')),
+                    SizedBox(height: 10.0,),
+                    Text('Network File Address:'),
+                    Text(' ${Constants.pdfURL}')
+                    
                   ],
                 ),
         ),
